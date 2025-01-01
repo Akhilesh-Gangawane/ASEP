5b066957142e0aac29d38b7163ca9e1ce8387a1f
Akhilesh Gangawane <akhi.gangawane1845@gmail.com>
2025-01-01 23:08:40 +0530

    Update user2_google_ai_studio.py

--- user2_google_ai_studio.py
+++ user2_google_ai_studio.py
@@ -10,7 +10,7 @@ absl.logging.set_stderrthreshold('error')
 
 
 
-genai.configure(api_key="AIzaSyDGHsBjiSbEK-DQ9ZcWEIFw1D0-MtJgOUk")
+genai.configure(api_key="")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("hi my name is akhilesh and not feeling well it feels like I'm lost and what to do don't know please give me step by step guide what to do ")
 print(response.text)
