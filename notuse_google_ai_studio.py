e2c9e37d0b994dc3e4e2f799d3c09d62332c4c5d
Akhilesh Gangawane <akhi.gangawane1845@gmail.com>
2025-01-01 23:07:49 +0530

    Update notuse_google_ai_studio.py

--- notuse_google_ai_studio.py
+++ notuse_google_ai_studio.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyDGHsBjiSbEK-DQ9ZcWEIFw1D0-MtJgOUk")
+genai.configure(api_key="")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Explain how AI works")
 print(response.text)
